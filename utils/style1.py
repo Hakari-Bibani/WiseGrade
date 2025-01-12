8c985dddf346044322a0e66ab3ee62405379077c
Hakari Bibani <hakary.jalal33@gmail.com>
2025-01-12 09:58:30 +0300

    Update style1.py

--- utils/style1.py
+++ utils/style1.py
@@ -10,6 +10,13 @@ def set_page_style():
         border: 1px solid #ccc;
         border-radius: 5px;
         padding: 10px;
+        background-color: #f9f9f9; /* Light gray background */
+    }
+    .stTextArea textarea {
+        background-color: #e6f7ff; /* Light blue background for the text area */
+        color: #333; /* Text color */
+        border: 1px solid #4da6ff; /* Blue border */
+        border-radius: 5px;
     }
     </style>
-    """, unsafe_allow_html=True)                        
+    """, unsafe_allow_html=True)
