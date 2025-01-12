71ea7430c2c219ab153d7b1e46568a1575ef72b1
Hakari Bibani <hakary.jalal33@gmail.com>
2025-01-12 09:14:06 +0300

    Update style1.py

--- utils/style1.py
+++ utils/style1.py
@@ -1,7 +1,22 @@
 import streamlit as st
 
+def apply_custom_styles():
 def set_page_style():
     st.markdown("""
+        <style>
+        input {
+            font-size: 16px;
+            padding: 5px;
+        }
+        textarea {
+            font-size: 16px;
+        }
+        button {
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: white;
+        }
+        </style>
     <style>
     body {
         font-family: 'Tiranti Solid Std Regular', cursive;
